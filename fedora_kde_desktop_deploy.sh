46fe71105c1871d1e07ab4529e3dbc6e264ad327
carolvs-mccallen <101008251+carolvs-mccallen@users.noreply.github.com>
2023-10-04 22:07:12 -0500

    Add files via upload

---
+++ fedora_kde_desktop_deploy.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+# Check if the script is run as root (sudo)
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script with sudo."
+  exit 1
+fi
+
+# Associative array to store set names and explanations
+declare -A sets
+sets["development"]="This set includes common development tools, PyCharm Community Edition, RStudio and Wireshark."
+sets["games"]="This set includes open source games, Heroic Launcher for Epic - GOG and Amazon games, and Steam."
+sets["matroska"]="This set includes video editing utilities for multiple formats including Matroska"
+sets["virt"]="This set includes RedHat Virtualization via Qemu and VirtualBox"
+sets["k3b"]="This set includes K3b and CD/DVD burning utilities"
+
+# Function to add repositories
+add_repositories() {
+  echo "Adding repositories..."
+  echo "RPMFusion"
+  dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  echo "Brave Browser"
+  dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+  rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+  echo "Microsoft VSCode and Edge"
+  rpm --import https://packages.microsoft.com/keys/microsoft.asc
+  sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+  dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
+  echo "Heroic Launcher"
+  dnf copr enable atim/heroic-games-launcher
+  echo "PyCharm Community"
+  dnf copr enable phracek/PyCharm
+  echo "Google Chrome"
+  dnf config-manager --set-enabled google-chrome
+  echo "Steam - RPMFusion"
+  dnf config-manager --set-enabled rpmfusion-nonfree-steam
+  echo "Nvidia Drivers - RPMFusion"
+  dnf config-manager --set-enabled rpmfusion-nonfree-nvidia-driver
+  echo "Flathub"
+  flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+  su -c "setsebool -P allow_execheap 1"
+}
+
+# Function to install a set of packages
+install_packages() {
+  local set_name="$1"
+  local packages=()
+
+  case "$set_name" in
+    "development")
+      packages=(code @development-tools pycharm-community pycharm-community-doc pycharm-community-plugins R rstudio-desktop wireshark)
+      ;;
+    "games")
+      packages=(astromenace frozen-bubble heroic-games-launcher-bin scummvm scummvm-data scummvm-tools steam supertux supertuxkart supertuxkart-data)
+      ;;
+    "matroska")
+      packages=(handbrake handbrake-gui mediainfo-gui mkvtoolnix-gui)
+      ;;
+    "virt")
+      packages=(akmod-VirtualBox kmod-VirtualBox qemu VirtualBox @Virtualization)
+      ;;
+    "k3b")
+      packages=(cdrskin k3b normalize sox transcode vcdimager xorriso)
+      ;;
+    *)
+      echo "Invalid set name. Exiting."
+      exit 1
+      ;;
+  esac
+
+  # Install the selected set of packages
+  echo "Installing $set_name packages..."
+  sudo dnf install --best --allowerasing -y "${packages[@]}"
+
+  # Check if the installation was successful
+  if [ $? -eq 0 ]; then
+    echo "$set_name installation successful."
+
+    # Run additional commands after set installation (if needed)
+    if [ "$set_name" == "virt" ]; then
+      echo "Completing virtualization packages setup..."
+      sudo usermod -aG vboxusers $USER
+    elif [ "$set_name" == "k3b" ]; then
+      echo "Completing K3b packages setup..."
+      sudo usermod -aG cdrom $USER
+    fi
+  else
+    echo "$set_name installation failed."
+  fi
+}
+
+# Function to install Flatpak apps
+install_flatpak_apps() {
+  read -p "Do you want to install Flatpak apps? (yes/no): " choice
+  case "$choice" in
+    [Yy]*)
+      echo "Installing Flatpak apps..."
+      flatpak install flathub org.gtk.Gtk3theme.Breeze com.bitwarden.desktop com.discordapp.Discord com.github.opentyrian.OpenTyrian com.plexamp.Plexamp tv.plex.PlexDesktop org.signal.Signal com.spotify.Client com.ktechpit.whatsie io.github.JaGoLi.ytdl_gui
+      echo "Applying automatic theme selection for Flatpak apps"
+      flatpak override --filesystem=xdg-config/gtk-3.0:ro
+      ;;
+    [Nn]*)
+      echo "No Flatpak apps will be installed."
+      ;;
+    *)
+      echo "Invalid choice. No Flatpak apps will be installed."
+      ;;
+  esac
+}
+
+# Function to install Popcorn Time
+install_popcorn_time() {
+  read -p "Do you want to install Popcorn Time? (yes/no): " choice
+  case "$choice" in
+    [Yy]*)
+      echo "Installing Popcorn Time..."
+      export PATH="$PATH:tmp"
+      wget https://github.com/popcorn-time-ru/popcorn-desktop/releases/download/v0.4.9/Popcorn-Time-0.4.9-linux64.zip
+      sudo mkdir /opt/popcorntime
+      sudo unzip Popcorn-Time-*-linux64.zip -d /opt/popcorntime/
+      sudo wget -O /opt/popcorntime/popcorn.png https://upload.wikimedia.org/wikipedia/commons/d/df/Pctlogo.png
+      sudo ln -sf /opt/popcorntime/Popcorn-Time /usr/bin/Popcorn-Time
+      echo "Creating app list"
+      sudo echo -e "[Desktop Entry]\nVersion=1.0\nType=Application\nTerminal=false\nName=Popcorn Time\nComment=Stream movies from the web\nExec=/usr/bin/Popcorn-Time\nIcon=/opt/popcorntime/popcorn.png\nCategories=AudioVideo;Player;Video" > /usr/share/applications/popcorntime.desktop
+      ;;
+    [Nn]*)
+      echo "Popcorn Time will not be installed."
+      ;;
+    *)
+      echo "Invalid choice. Popcorn Time will not be installed."
+      ;;
+  esac
+}
+
+# Running pre-requisite upgrade
+sudo dnf update -y
+
+# Add repositories and run commands before package selection
+add_repositories
+
+# Initial installation
+echo "Updating package repository and installing initial packages..."
+sudo dnf update -y
+sudo dnf install --best --allowerasing -y arj brave-browser btrfs-assistant btrfsmaintenance cabextract digikam dnf-utils dolphin-megasync dpkg dropbox fprintd-devel gimp gimp-data-extras gimp-*-plugin gimp-elsamuko gimp-*-filter gimp-help gimp-help-es gimp-layer* gimp-lensfun gimp-*-masks gimp-resynthesizer gimp-save-for-web gimp-separate+ gimp-*-studio gimp-wavelet* gimpfx-foundry git git-core google-chrome-stable htop hunspell hunspell-es info innoextract kate kde-l10n-es kdiskmark kernel-devel kernel-headers kget kid3 kleopatra krename krita krusader ksystemlog ktorrent lha libcurl-devel libreoffice-langpack-es libreoffice-help-es libfprint-devel libxml2-devel lshw megasync microsoft-edge-stable mozilla-ublock-origin neofetch nextcloud-client nextcloud-client-dolphin openssl-devel okteta perl pstoedit redhat-lsb-core snapper telegram-desktop tracker unace unrar vlc vlc-bittorrent vlc-extras xkill
+
+# Check if the initial installation was successful
+if [ $? -eq 0 ]; then
+  echo "Initial installation successful."
+else
+  echo "Initial installation failed."
+  exit 1
+fi
+
+# Prompt user for additional installations
+while true; do
+  echo "Available sets:"
+  for set_name in "${!sets[@]}"; do
+    echo "$set_name - ${sets[$set_name]}"
+  done
+
+  read -p "Enter the set of packages you want to install or 'exit' to quit: " choice
+
+  if [ "$choice" == "exit" ]; then
+    echo "Exiting."
+    break
+  fi
+
+  if [ -n "${sets[$choice]}" ]; then
+    install_packages "$choice"
+  else
+    echo "Invalid set name. Please choose from the available sets."
+  fi
+done
+
+# Install Flatpak apps
+install_flatpak_apps
+
+# Install Popcorn Time
+install_popcorn_time
